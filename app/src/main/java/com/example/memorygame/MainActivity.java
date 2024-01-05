6804aff962683df0399ed705e7a52f6981f4870b
adampridor <adam@pridor.com>
2024-01-05 11:19:38 +0200

    16 image views and on new game function

--- app/src/main/java/com/example/memorygame/MainActivity.java
+++ app/src/main/java/com/example/memorygame/MainActivity.java
@@ -5,10 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    int [][] board;
+
+    int [] pic;
+
+    private int counter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void onNewGame() {
+        board = new int[4][4];
+        for (int row=0; row < 4; row++)
+            for (int col=0; col < 4; col++)
+                board[row][col] = new int();
     }
 }
